db828c08a32db1aaac2c92accfcf3d010f9705c3
PranavRasal <pranavcode19@gmail.com>
2026-02-23 12:01:00 +0530

    feat: integrate Tailwind CSS into the project
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed existing CSS styles in App.css and replaced them with Tailwind CSS imports.
    - Updated App.jsx to utilize Tailwind CSS classes for styling.
    - Removed index.css as it contained redundant styles.

--- mega-blog-with-appWrite/src/App.jsx
+++ mega-blog-with-appWrite/src/App.jsx
@@ -3,13 +3,13 @@ import {useDispatch} from 'react-redux'
 import './App.css'
 
 function App() {
- const[loading,setLoading] = useState(true)
- const dispatch = useDispatch()
+//  const[loading,setLoading] = useState(true)
+//  const dispatch = useDispatch()
 
   return (
 
     <>
-      <h1>Vite  <br></br>React</h1>
+      <h1 className='text-3xl font-bold text-center'>Vite  <br></br>React</h1>
     </>
   )
 }
